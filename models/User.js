25a0a16b4941220fa7e4a384ef55bc032c7cc28d
vucuong12 <vucuong020195@gmail.com>
2016-10-08 22:45:52 +0900

    add landing page

--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
+	
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
