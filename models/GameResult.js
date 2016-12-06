0e20fb34f7bf3c1eb1f9a857577184699d4e57f9
vucuong12 <vucuong020195@gmail.com>
2016-12-06 21:52:55 +0900

    add GameResult and other small things

--- models/GameResult.js
+++ models/GameResult.js
@@ -10,12 +10,17 @@ var GameResult = new keystone.List('GameResult');
 GameResult.add({
 	userID: {type: Types.Text},
 	mapID: {type: Types.Text},
+	userNickname: {type: Types.Text},
 	createdAt: { type: Types.Datetime, default: Date.now },
-	isCompleted: {type: Types.Boolean}, //
+	isCompleted: {type: Types.Boolean, default: false}, //
 	language: {type: Types.Text},
 	timeToFinish: {type: Types.Number}, //second
 	latestSubmissionID: {type: Types.Text},
 	bestSubmissionID: {type: Types.Text}, 
+});
+
+GameResult.schema.add({
+    createdTime : { type : Date, default: Date.now }
 });
 
 
